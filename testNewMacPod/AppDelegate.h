0517ef1340f854a0a376dbf9588f5e3f7003ae79
爱恨的潮汐 <497266156@qq.com>
2018-07-26 14:04:31 +0800

    Initial Commit

---
+++ testNewMacPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testNewMacPod
+//
+//  Created by 爱恨的潮汐 on 2018/7/26.
+//  Copyright © 2018年 AiHenDeChaoXi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testNewMacPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testNewMacPod
+//
+//  Created by 爱恨的潮汐 on 2018/7/26.
+//  Copyright © 2018年 AiHenDeChaoXi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
